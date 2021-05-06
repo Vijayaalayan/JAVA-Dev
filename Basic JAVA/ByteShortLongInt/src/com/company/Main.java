8abb05fcbdbfbc06105f2939ea991a6a118c325b
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-06 11:11:44 +0530

    Integer MIN & MAX

--- Basic JAVA/ByteShortLongInt/src/com/company/Main.java
+++ Basic JAVA/ByteShortLongInt/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int minInt = Integer.MIN_VALUE;
+	    int maxInt = Integer.MAX_VALUE;
+	    System.out.println(minInt);
+        System.out.println(maxInt);
+
+//        overflow and underflow
+        System.out.println(minInt-1);
+        System.out.println(maxInt+1);
+
+        int number = 2_12_21_21;
+        System.out.println(number);
+
     }
 }
