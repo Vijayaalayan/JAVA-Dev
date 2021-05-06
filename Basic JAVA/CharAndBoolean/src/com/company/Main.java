cd02c4b7b529879b27cb2388babfbd209e597036
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-06 23:30:37 +0530

    CharAndBoolean

---
+++ Basic JAVA/CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = 'D';
+	    char unicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(unicodeChar);
+
+        boolean _true = true;
+        boolean _false = false;
+        System.out.println(_true);
+        System.out.println(_false);
+    }
+}
