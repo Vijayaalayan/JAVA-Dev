b5fc957b7dde7548db8beb1df10292e7d7f9932f
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-06 16:37:29 +0530

    working with MinMax of ByteShortLongInt

--- Basic JAVA/ByteShortLongInt/src/com/company/Main.java
+++ Basic JAVA/ByteShortLongInt/src/com/company/Main.java
@@ -15,5 +15,23 @@ public class Main {
         int number = 2_12_21_21;
         System.out.println(number);
 
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println(minByte);
+        System.out.println(maxByte);
+
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        System.out.println(minShort);
+        System.out.println(maxShort);
+
+        long myLong = 10046346334L;
+        System.out.println(myLong);
+        long minLong = Long.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+        System.out.println(minLong);
+        System.out.println(maxLong);
+        
+
     }
 }
