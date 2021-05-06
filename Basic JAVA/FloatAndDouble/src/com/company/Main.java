e21131905f14b5435c47faede765862cc75578b0
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-06 23:15:23 +0530

    inital FloatAndDouble

---
+++ Basic JAVA/FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float minVal = Float.MIN_VALUE;
+	    float maxVal = Float.MAX_VALUE;
+        System.out.println(minVal);
+        System.out.println(maxVal);
+
+        double minDVal = Double.MIN_VALUE;
+        double maxDVal = Double.MAX_VALUE;
+        System.out.println(minDVal);
+        System.out.println(maxDVal);
+
+        int x = 10;
+        float y = (float)5.25;
+        double z = 10.23;
+    }
+}
