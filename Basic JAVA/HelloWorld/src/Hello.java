fbda4bebbf5f53e99ab1a615813d96339879f31c
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-06 10:56:40 +0530

    final commit - HelloWorld

--- Basic JAVA/HelloWorld/src/Hello.java
+++ Basic JAVA/HelloWorld/src/Hello.java
@@ -1,2 +1,7 @@
 public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Vijay");
+        int firstNumber = 5;
+        System.out.println(firstNumber);
+    }
 }
