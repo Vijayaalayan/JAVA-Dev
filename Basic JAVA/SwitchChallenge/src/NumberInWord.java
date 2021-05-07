738877b11daf66cec1fce49a829788382a4ed8e0
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-07 23:14:44 +0530

    LeapYear - Days - Challenge 2

--- Basic JAVA/SwitchChallenge/src/NumberInWord.java
+++ Basic JAVA/SwitchChallenge/src/NumberInWord.java
@@ -35,4 +35,63 @@ public class NumberInWord {
                 System.out.println("OTHER");
         }
     }
+
+    public static boolean isLeapYear(int year){
+        if(year<1 || year>9999){
+            return false;
+        }
+        if(year%400 == 0 ){
+            return true;
+        }
+        if(year%100 == 0){
+            return false;
+        }
+        if(year%4 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static int getDaysInMonth(int month,int year){
+        if(month<1 || month>12){
+            return -1;
+        }
+        if(year<1 || year>9999){
+            return -1;
+        }
+        boolean result = isLeapYear(year);
+        if(result){
+            if(month == 2){
+                return 29;
+            }else if(month<8){
+                if(month%2 == 0){
+                    return 30;
+                }else{
+                    return 31;
+                }
+            }else{
+                if(month%2 == 0){
+                    return 31;
+                }else{
+                    return 30;
+                }
+            }
+        }else{
+            if(month == 2){
+                return 28;
+            }else if(month<8){
+                if(month%2 == 0){
+                    return 30;
+                }else{
+                    return 31;
+                }
+            }else{
+                if(month%2 == 0){
+                    return 31;
+                }else{
+                    return 30;
+                }
+            }
+        }
+    }
 }
