864ffc3fab7b247c770dc62b0172343a9f99dfe9
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-07 11:13:20 +0530

    MegaBytesConverter Challenge

---
+++ Basic JAVA/MegaBytesConvertChallenge/src/Convert.java
@@ -0,0 +1,11 @@
+public class Convert {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes<0){
+            System.out.println("Invalid Value");
+        }else{
+            int mb = kiloBytes/1024;
+            int kb = kiloBytes - (mb*1024);
+            System.out.println(kiloBytes+" KB = "+mb+" MB and "+kb+" KB");
+        }
+    }
+}
