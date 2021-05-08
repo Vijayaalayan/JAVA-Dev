411c807d9f28c86f4c5e3ea3b8160e93884d7920
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-08 22:01:49 +0530

    UserInput

---
+++ Basic JAVA/UserInput/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter year of birth : ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();  //Handle next line character
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+        int age = 2021 - yearOfBirth;
+        System.out.println("Your name is "+name+" and you are "+age+" years old");
+    }
+}
