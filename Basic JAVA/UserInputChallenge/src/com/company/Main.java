9c7fdf36f4f4acd959f2b772c499e7d3260ba6f3
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-08 22:52:43 +0530

    Coding Challenge - 27

--- Basic JAVA/UserInputChallenge/src/com/company/Main.java
+++ Basic JAVA/UserInputChallenge/src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
-import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double avg = 0;
+        int count = 0;
+        while (true){
+            boolean isInt = scanner.hasNextInt();
+            if(isInt){
+                int number = scanner.nextInt();
+                count++;
+                sum+=number;
+            }else{
+                avg = Math.ceil(sum/count);
+                System.out.println("SUM = "+(int)sum+" AVG = "+(int)avg);
+                break;
+            }
+            scanner.nextLine();
+        }
+    }
 
     public static void main(String[] args) {
 //        Challenge 1
@@ -24,26 +43,29 @@ public class Main {
 //        System.out.println(sum);
 
 //        Challenge 2
-        Scanner scanner = new Scanner(System.in);
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        while(true){
-            System.out.println("Enter a number");
-            boolean isInt = scanner.hasNextInt();
-            if(isInt){
-                int number = scanner.nextInt();
-                if(min>number){
-                    min = number;
-                }
-                if(max<number){
-                    max = number;
-                }
-            }else {
-                break;
-            }
-            scanner.nextLine();
-        }
-        System.out.println("Minimum is : "+min);
-        System.out.println("Maximum is : "+max);
+//        Scanner scanner = new Scanner(System.in);
+//        int min = Integer.MAX_VALUE;
+//        int max = Integer.MIN_VALUE;
+//        while(true){
+//            System.out.println("Enter a number");
+//            boolean isInt = scanner.hasNextInt();
+//            if(isInt){
+//                int number = scanner.nextInt();
+//                if(min>number){
+//                    min = number;
+//                }
+//                if(max<number){
+//                    max = number;
+//                }
+//            }else {
+//                break;
+//            }
+//            scanner.nextLine();
+//        }
+//        System.out.println("Minimum is : "+min);
+//        System.out.println("Maximum is : "+max);
+
+//        Coding Exercise - 27
+        inputThenPrintSumAndAverage();
     }
 }
