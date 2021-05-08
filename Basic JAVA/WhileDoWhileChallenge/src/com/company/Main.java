d33a67cc507e58bec80c37cdf549b06d350cf822
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-08 15:54:45 +0530

    Coding Challenge - 17

--- Basic JAVA/WhileDoWhileChallenge/src/com/company/Main.java
+++ Basic JAVA/WhileDoWhileChallenge/src/com/company/Main.java
@@ -14,7 +14,22 @@ public class Main {
         }
         return sum;
     }
-    
+
+    //  Coding Exercise 17
+    public static int getEvenDigitSum(int number){
+        if(number<0){
+            return -1;
+        }
+        int sum = 0;
+        while(number>0){
+            int temp = number%10;
+            if(temp%2 == 0){
+                sum+=temp;
+            }
+            number/=10;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
 	    int sum = sumDigits(21);
