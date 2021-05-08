d04f4e08614810743cefb8fbc8dec01629a1957a
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-08 22:39:53 +0530

    Challenge -2

--- Basic JAVA/UserInputChallenge/src/com/company/Main.java
+++ Basic JAVA/UserInputChallenge/src/com/company/Main.java
@@ -1,24 +1,49 @@
 package com.company;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+//        Challenge 1
+//        Scanner scanner = new Scanner(System.in);
+//        int count = 0;
+//        int sum = 0;
+//        while(count<10){
+//            boolean hasNextInt = scanner.hasNextInt();
+//            if(hasNextInt){
+//                int temp = scanner.nextInt();
+//                count++;
+//                sum+=temp;
+//            }else{
+//                System.out.println("Invalid");
+//            }
+//            scanner.nextLine();
+//        }
+//        System.out.println(sum);
+
+//        Challenge 2
         Scanner scanner = new Scanner(System.in);
-        int count = 0;
-        int sum = 0;
-        while(count<10){
-            boolean hasNextInt = scanner.hasNextInt();
-            if(hasNextInt){
-                int temp = scanner.nextInt();
-                count++;
-                sum+=temp;
-            }else{
-                System.out.println("Invalid");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        while(true){
+            System.out.println("Enter a number");
+            boolean isInt = scanner.hasNextInt();
+            if(isInt){
+                int number = scanner.nextInt();
+                if(min>number){
+                    min = number;
+                }
+                if(max<number){
+                    max = number;
+                }
+            }else {
+                break;
             }
             scanner.nextLine();
         }
-        System.out.println(sum);
+        System.out.println("Minimum is : "+min);
+        System.out.println("Maximum is : "+max);
     }
 }
