ae1807a160adbc403a638946df1149dcab523cf1
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-08 22:09:12 +0530

    Resolving error

--- Basic JAVA/UserInput/src/com/company/Main.java
+++ Basic JAVA/UserInput/src/com/company/Main.java
@@ -7,11 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter year of birth : ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();  //Handle next line character
-        System.out.println("Enter your name : ");
-        String name = scanner.nextLine();
-        int age = 2021 - yearOfBirth;
-        System.out.println("Your name is "+name+" and you are "+age+" years old");
+        boolean hasNextInt = scanner.hasNextInt();
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();  //Handle next line character
+            System.out.println("Enter your name : ");
+            String name = scanner.nextLine();
+            int age = 2021 - yearOfBirth;
+            if(age>=0 && age<=100){
+                System.out.println("Your name is "+name+" and you are "+age+" years old");
+            }else{
+                System.out.println("Invalid Year of Birth");
+            }
+
+        }else{
+            System.out.println("Unable to parse year of birth");
+        }
+
     }
 }
