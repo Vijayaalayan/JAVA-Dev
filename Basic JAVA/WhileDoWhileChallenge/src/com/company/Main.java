395f184e29cc17186305e7dbfb5978fff48f2e8f
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-08 16:01:17 +0530

    Coding Challenge - 19

--- Basic JAVA/WhileDoWhileChallenge/src/com/company/Main.java
+++ Basic JAVA/WhileDoWhileChallenge/src/com/company/Main.java
@@ -31,6 +31,21 @@ public class Main {
         return sum;
     }
 
+    public static boolean isValid(int number){
+        return number > 9 && number <= 1000;
+    }
+
+    public static boolean hasSameLastDigit(int n1,int n2,int n3){
+        if(isValid(n1) && isValid(n2) && isValid(n3)){
+            int t1 = n1%10;
+            int t2 = n2%10;
+            int t3 = n3%10;
+            return t1 == t2 || t2 == t3 || t1 == t3;
+        }else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
 	    int sum = sumDigits(21);
         System.out.println(sum);
