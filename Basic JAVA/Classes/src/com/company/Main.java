7236e3b42112718b3cb46de4e5475eb448631273
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-09 13:04:17 +0530

    Coding Challenge - 29

--- Basic JAVA/Classes/src/com/company/Main.java
+++ Basic JAVA/Classes/src/com/company/Main.java
@@ -3,14 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Food food = new Food();
+//	    Food food = new Food();
 
 //	    When name declared as public
 //	    food.name = "Maggi";
 //        System.out.println(food.name);
 
 //       When name declared as private
-        food.setName("Noodles");
-        food.getName();
+//        food.setName("Noodles");
+//        food.getName();
+
+//       Coding Exercise - 29
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println(calculator.getAdditionResult());
+        System.out.println(calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println(calculator.getMultiplicationResult());
+        System.out.println(calculator.getDivisionResult());
     }
 }
