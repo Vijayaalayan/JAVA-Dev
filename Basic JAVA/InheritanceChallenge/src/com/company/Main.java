b67a28095ecf7f9b13f766779194c4451f96d575
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-10 13:13:49 +0530

    Coding Challenge - 36

--- Basic JAVA/InheritanceChallenge/src/com/company/Main.java
+++ Basic JAVA/InheritanceChallenge/src/com/company/Main.java
@@ -11,13 +11,25 @@ public class Main {
 //        swift.steer(24);
 
 //        Coding Challenge - 35
-        Circle circle = new Circle(3.75);
-        System.out.println(circle.getRadius());
-        System.out.println(circle.getArea());
-        Cylinder cylinder = new Cylinder(5.55,7.25);
-        System.out.println(cylinder.getRadius());
-        System.out.println(cylinder.getHeight());
-        System.out.println(cylinder.getArea());
-        System.out.println(cylinder.getVolume());
+//        Circle circle = new Circle(3.75);
+//        System.out.println(circle.getRadius());
+//        System.out.println(circle.getArea());
+//        Cylinder cylinder = new Cylinder(5.55,7.25);
+//        System.out.println(cylinder.getRadius());
+//        System.out.println(cylinder.getHeight());
+//        System.out.println(cylinder.getArea());
+//        System.out.println(cylinder.getVolume());
+
+//        Coding Challenge - 36
+        Rectangle rectangle = new Rectangle(5,10);
+        System.out.println(rectangle.getWidth());
+        System.out.println(rectangle.getLength());
+        System.out.println(rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println(cuboid.getWidth());
+        System.out.println(cuboid.getLength());
+        System.out.println(cuboid.getArea());
+        System.out.println(cuboid.getHeight());
+        System.out.println(cuboid.getVolume());
     }
 }
