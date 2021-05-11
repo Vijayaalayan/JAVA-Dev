4106cb797874e84fcbccf4d48702d0e4cc529bf9
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-11 20:11:37 +0530

    Coding Challenge - 38

--- Basic JAVA/Encapsulation/src/com/company/Main.java
+++ Basic JAVA/Encapsulation/src/com/company/Main.java
@@ -12,6 +12,14 @@ public class Main {
 //        player.health+=40;
 //        System.out.println(player.getHealth());
 
+//Coding Challenge - 38
+        Printer printer = new Printer(50,true);
+        System.out.println(printer.addToner(50));
+        System.out.println(printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println(pagesPrinted+" "+printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println(pagesPrinted+" "+printer.getPagesPrinted());
 
     }
 }
