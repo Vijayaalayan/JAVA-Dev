843c9c8d9bdb0475e8df26e0b5631ebe4d9315f3
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-13 23:30:37 +0530

    Reference vs Value

---
+++ Basic JAVA/Array/src/com/company/ReferenceVsValue.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class ReferenceVsValue {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = num1;
+        System.out.println(num1);
+        System.out.println(num2);
+        num2 = 10;
+        System.out.println(num1);
+        System.out.println(num2);
+        int[] arr1 = new int[5];
+        for(int i=0;i<5;i++){
+            arr1[i] = i;
+        }
+        int[] arr2 = arr1;
+        for(int i=0;i<5;i++){
+            System.out.print(arr1[i]+" ");
+        }
+        System.out.println();
+        for(int i=0;i<5;i++){
+            System.out.print(arr2[i]+" ");
+        }
+        System.out.println();
+        arr2[3] = 10;
+        for(int i=0;i<5;i++){
+            System.out.print(arr1[i]+" ");
+        }
+        System.out.println();
+        for(int i=0;i<5;i++){
+            System.out.print(arr2[i]+" ");
+        }
+    }
+}
