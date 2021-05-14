b73e5371e02e49a5d9d9822e08f8456a778aa336
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-14 21:05:46 +0530

    Arrays to String

--- Basic JAVA/Array/src/com/company/ReferenceVsValue.java
+++ Basic JAVA/Array/src/com/company/ReferenceVsValue.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class ReferenceVsValue {
     public static void main(String[] args) {
         int num1 = 5;
@@ -30,5 +32,7 @@ public class ReferenceVsValue {
         for(int i=0;i<5;i++){
             System.out.print(arr2[i]+" ");
         }
+        System.out.println();
+        System.out.println("Array 1 :"+ Arrays.toString(arr1));
     }
 }
