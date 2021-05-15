024e07bd1d820587205be4b607777a4418c4e9d4
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-15 22:34:34 +0530

    Create GroceryList.java

---
+++ Basic JAVA/ArrayList/src/com/company/GroceryList.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<>();
+    public void addGroceryItem(String item){
+        groceryList.add(item);
+    }
+    public void printList(){
+        System.out.println("You have "+groceryList.size()+" items in your grocery list");
+        for(int i=0;i<groceryList.size();i++){
+            System.out.println((i+1)+". "+groceryList.get(i));
+        }
+    }
+    public void modifyGroceryList(int position, String item){
+        groceryList.set(position,item);
+    }
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+    public int findItem(String searchItem){
+//        boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if(position>=0){
+            return position;
+        }
+        return -1;
+    }
+}
