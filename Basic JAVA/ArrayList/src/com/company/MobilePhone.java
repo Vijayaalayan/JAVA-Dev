25cd3f73771c3fa3b618672f31d201886e9e03cb
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-16 21:49:27 +0530

    Update MobilePhone.java

--- Basic JAVA/ArrayList/src/com/company/MobilePhone.java
+++ Basic JAVA/ArrayList/src/com/company/MobilePhone.java
@@ -1,4 +1,46 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact){
+        if(findContact(contact.getName())>=0){
+            System.out.println("Contact already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact,Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition<0){
+            System.out.println(oldContact.getName()+" not found");
+            return false;
+        }
+        this.myContacts.set(foundPosition,newContact);
+        System.out.println(oldContact.getName()+" is replaced with "+newContact.getName());
+        return true;
+    }
+
+    private int findContact(Contact contact){
+        return this.myContacts.indexOf(contact);
+    }
+    private int findContact(String name){
+        for(int i=0;i<this.myContacts.size();i++){
+            Contact contact = this.myContacts.get(i);
+            if(contact.getName().equals(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
