c67e4321c650aac589885a4e9babdd797ca95b84
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-20 22:13:36 +0530

    Update MobileMain.java

--- Basic JAVA/ArrayList/src/com/company/MobileMain.java
+++ Basic JAVA/ArrayList/src/com/company/MobileMain.java
@@ -17,16 +17,52 @@ public class MobileMain {
                     flag = false;
                     break;
                 case 1:
-                    printContacts();
+                    mobilePhone.printContacts();
                     break;
                 case 2:
-                    
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+                default:
+                    printActions();
+                    System.out.println("Enter between 0 and 6");
             }
         }
     }
 
-    private static void printContacts() {
+    private static void queryContact() {
+    }
 
+    private static void removeContact() {
+        
+    }
+
+    private static void updateContact() {
+        
+    }
+
+    private static void addNewContact() {
+        System.out.println("Enter new contact name : ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number : ");
+        String number = scanner.nextLine();
+        Contact newContact = new Contact(name,number);
+        if(mobilePhone.addNewContact(newContact)){
+            System.out.println("New contact added : Name = "+name+" Number = "+number);
+        }else{
+            System.out.println("Cannot add, "+name+" already on file");
+        }
     }
 
     static void startPhone(){
@@ -38,9 +74,9 @@ public class MobileMain {
         System.out.println("0 - to shutdown\n"+
                            "1 - to print contacts\n"+
                            "2 - to add a new contact\n"+
-                           "3 - to update an existaing contact\n"+
+                           "3 - to update an existing contact\n"+
                            "4 - to remove a contact\n"+
-                           "5 - query if a contacct exists\n"+
+                           "5 - query if a contact exists\n"+
                            "6 - to print the list of all available actions");
         System.out.println("Choose your action : ");
     }
