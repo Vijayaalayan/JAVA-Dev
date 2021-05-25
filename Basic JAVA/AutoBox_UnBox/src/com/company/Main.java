4ea3d29cdf952b94449b5adb1c43e9a89a722d76
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-25 14:23:52 +0530

    AutoBox_UnBox

---
+++ Basic JAVA/AutoBox_UnBox/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.ArrayList;
+
+class IntClass{
+    private int i;
+    public IntClass(int i){
+        this.i=i;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            arrayList.add(i);
+        }
+        for(int i=0;i<10;i++){
+            System.out.println(i+"-->"+ arrayList.get(i));
+        }
+
+        ArrayList<IntClass> intclass = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            intclass.add(new IntClass(i));
+        }
+        for(int i=0;i<10;i++){
+            System.out.println(intclass.get(i).getI());
+        }
+    }
+}
