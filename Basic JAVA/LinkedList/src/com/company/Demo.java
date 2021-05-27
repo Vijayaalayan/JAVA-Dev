d3529a1929c5261a32f071db24387163f75bb3fd
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-27 18:48:25 +0530

    linkedlist - iterator -> next,previous

--- Basic JAVA/LinkedList/src/com/company/Demo.java
+++ Basic JAVA/LinkedList/src/com/company/Demo.java
@@ -1,22 +1,29 @@
 package com.company;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> linkedList = new LinkedList<>();
-        linkedList.add("Vijay");
-        linkedList.add("Nithin");
-        linkedList.add("Naveen");
+//        linkedList.add("Vijay");
+//        linkedList.add("Nithin");
+//        linkedList.add("Naveen");
+//
+//        printList(linkedList);
+//
+//        linkedList.add(0,"Kili");
+//        printList(linkedList);
+//
+//        linkedList.remove("Kili");
+//        printList(linkedList);
+        addInorder(linkedList,"Vijay");
+        addInorder(linkedList,"Naveen");
+        addInorder(linkedList,"Nithin");
+        addInorder(linkedList,"Kili");
 
         printList(linkedList);
 
-        linkedList.add(0,"Kili");
-        printList(linkedList);
-
-        linkedList.remove("Kili");
-        printList(linkedList);
+        visit(linkedList);
     }
     private static void printList(LinkedList<String> linkedList){
         Iterator<String> it = linkedList.iterator();
@@ -25,4 +32,76 @@ public class Demo {
         }
         System.out.println("=======================");
     }
+    
+    private static boolean addInorder(LinkedList<String> linkedList,String newMember){
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+        while(stringListIterator.hasNext()){
+            int comparison = stringListIterator.next().compareTo(newMember);
+            if(comparison == 0){
+                System.out.println(newMember+" already exists");
+                return false;
+            }else if(comparison>0){
+                stringListIterator.previous();
+                stringListIterator.add(newMember);
+                return true;
+            }else if(comparison<0) {
+
+            }
+        }
+        stringListIterator.add(newMember);
+        return true;
+    }
+
+    private static void visit(LinkedList<String> members){
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        ListIterator<String> listIterator = members.listIterator();
+        if(members.isEmpty()){
+            System.out.println("No members is list");
+            return;
+        }else{
+            System.out.println("Now visiting --> "+listIterator.next());
+            printMenu();
+        }
+        while(flag){
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action){
+                case 0:
+                    System.out.println("Exiting the list");
+                    flag = false;
+                    break;
+                case 1:
+                    if(listIterator.hasNext()){
+                        System.out.println("Now visiting --> "+listIterator.next());
+                    }else{
+                        System.out.println("Reached end of list");
+                    }
+                    break;
+                case 2:
+                    if(listIterator.hasPrevious()){
+                        System.out.println("Now visiting --> "+listIterator.previous());
+                    }else{
+                        System.out.println("Reached end of list");
+                    }
+                    break;
+                case 3:
+                    printMenu();
+                    break;
+                default:
+                    System.out.println("Please select from available actions");
+                    printMenu();
+            }
+        }
+
+    }
+
+    private static void printMenu(){
+        System.out.println("Available actions\nPress " +
+                "0 - to quit\n" +
+                "1 - to go next\n" +
+                "2 - to go previous\n" +
+                "3 - to print menu");
+    }
+
 }
