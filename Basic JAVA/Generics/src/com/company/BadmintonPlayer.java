27bc9428fd3a7370ab8ef7091a7f6019ecf8c702
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-28 13:11:14 +0530

    Generics - 1

---
+++ Basic JAVA/Generics/src/com/company/BadmintonPlayer.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class BadmintonPlayer extends Player{
+    public BadmintonPlayer(String name) {
+        super(name);
+    }
+}
--- Basic JAVA/Generics/src/com/company/Main.java
+++ Basic JAVA/Generics/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        CricketPlayer cricketPlayer = new CricketPlayer("Mahendra Singh Dhoni");
+        FootballPlayer footballPlayer = new FootballPlayer("Sunil Chetri");
+        BadmintonPlayer badmintonPlayer = new BadmintonPlayer("Kidambi Srikanth");
 
+        Team mumbai_indians = new Team("Mumbai Indians");
+        mumbai_indians.addPlayer(cricketPlayer);
+        mumbai_indians.addPlayer(footballPlayer);
+        mumbai_indians.addPlayer(badmintonPlayer);
+
+        System.out.println(mumbai_indians.numPlayers());
     }
 }
