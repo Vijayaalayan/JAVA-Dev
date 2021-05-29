a2d92f2c7d688b23c1ebd4ce2a46843e59a527b7
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-29 13:33:25 +0530

    Challenge Complete

--- Basic JAVA/PackageTestChallenge/src/com/company/Main.java
+++ Basic JAVA/PackageTestChallenge/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.example.mylibrary.Series;
+
 public class Main {
 
     public static void main(String[] args) {
-	    
+        System.out.println(Series.nSum(10));
+        System.out.println(Series.nSum(10));
+        Series.fibanocci(10);
     }
 }
