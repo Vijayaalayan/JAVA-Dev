21ef913e83b866e485bbe922450908545a903eb8
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-05-30 14:07:35 +0530

    Update Password.java

--- Basic JAVA/Final/src/com/company/Password.java
+++ Basic JAVA/Final/src/com/company/Password.java
@@ -12,7 +12,8 @@ public class Password {
         return password*key;
     }
 
-    public void storePassword(){
+//    if declared as final no other method can override and change the critical information
+    public final void storePassword(){
         System.out.println("Saving password as "+this.encrytptedPassword);
     }
 
