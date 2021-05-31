f06f719eb2d844dbd7bbd79780cbaa3cb3622e0d
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-06-01 02:12:52 +0530

    SAME - without using fxml

--- JavaFX/hello/src/sample/Main.java
+++ JavaFX/hello/src/sample/Main.java
@@ -2,17 +2,23 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root,300,275));
         primaryStage.show();
     }
 
