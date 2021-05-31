94339868a31f430ba4d845641c6197414e4fd8e2
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-06-01 02:29:25 +0530

    Label code

--- JavaFX/hello/src/sample/Main.java
+++ JavaFX/hello/src/sample/Main.java
@@ -5,7 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,8 +22,15 @@ public class Main extends Application {
         root.setAlignment(Pos.CENTER);
         root.setHgap(10);
         root.setVgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX");
+//        greeting.setTextFill(Paint.valueOf("0000FF"));
+        greeting.setTextFill(Color.RED);
+        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD,70));
+        root.getChildren().add(greeting);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root,300,275));
+        primaryStage.setScene(new Scene(root,700,275));
         primaryStage.show();
     }
 
