3d558826bf359a93f34d9ff8a4d6a0f725a6ecaa
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-06-04 20:24:47 +0530

    Button click @FXML

--- JavaFX/Events/src/sample/Controller.java
+++ JavaFX/Events/src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField nameField;;
+    public void onButtonClicked(){
+        System.out.println(nameField.getText()+", you have clicked me");
+    }
 }
