3d5accab1beb9b300aa82cd68f3113f6561f57d5
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-06-08 04:17:52 +0530

    completed dialogpane robust

--- JavaFX/TodoList/src/com/vijay/todolist/Controller.java
+++ JavaFX/TodoList/src/com/vijay/todolist/Controller.java
@@ -6,15 +6,11 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
