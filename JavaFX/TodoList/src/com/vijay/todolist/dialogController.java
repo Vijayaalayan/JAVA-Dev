b30f4eb1c5ec23589aca3cb61368ef0eca4c595b
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-06-08 04:12:11 +0530

    processing Dialog

--- JavaFX/TodoList/src/com/vijay/todolist/dialogController.java
+++ JavaFX/TodoList/src/com/vijay/todolist/dialogController.java
@@ -1,4 +1,27 @@
 package com.vijay.todolist;
 
-public class dialogController {
+import com.vijay.todolist.datamodel.TodoData;
+import com.vijay.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsField;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public void processResults(){
+        String shortDesctiption = shortDescriptionField.getText().trim();
+        String details = detailsField.getText().trim();
+        LocalDate date = deadlinePicker.getValue();
+
+        TodoData.getInstance().addTodoItem(new TodoItem(shortDesctiption,details,date));
+    }
 }
