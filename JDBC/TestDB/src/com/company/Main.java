0fbe274a4c8e4fff5f57fd57f1ea8345a142e1dd
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-06-20 19:07:48 +0530

    insert query

--- JDBC/TestDB/src/com/company/Main.java
+++ JDBC/TestDB/src/com/company/Main.java
@@ -10,8 +10,13 @@ public class Main {
     public static void main(String[] args) {
 	    try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\JAVA-Dev\\JDBC\\TestDB\\testdb.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS contacts(name TEXT,phone INTEGER ,email TEXT)");
+            statement.execute("INSERT into contacts(name,phone,email) values ('Vijay',1234,'vijay@gmail.com')");
+            statement.execute("INSERT into contacts(name,phone,email) values ('Naveen',2341,'naveen@gmail.com')");
+            statement.execute("INSERT into contacts(name,phone,email) values ('Nithin',3412,'nithin@gmail.com')");
+            
             statement.close();
             conn.close();
         }catch (SQLException e){
